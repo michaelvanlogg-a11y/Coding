83aa40173b51607830abe5f586d776b26b49d615
michaelvanlogg-a11y <michael.vanlogg@papamoacollege.school.nz>
2026-02-26 11:57:16 +1300

    Add files via upload

---
+++ Year 11 coding/C_01_yes_no_basic_V3.py
@@ -0,0 +1,27 @@
+
+# functions go here
+
+def yes_no(question):
+
+    " " "Checks user response to a question is yes / no (y/n) , return 'yes' or 'no' " " "
+
+    while True:
+    
+        response = input(question).lower()
+
+        # check the user says  yes / no / y / n
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n": 
+            return "no"
+        else:
+            print("please enter yes / no")
+
+# Main routine
+
+# testing loop
+while True:
+    want_instructions = yes_no(" Do you want to see the instructions")
+    print(f"you choose {want_instructions}")
+
+    print("we done")
