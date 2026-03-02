e63eeaea24f18b2a1b5bbfa2d4b264fc24bfc629
michaelvanlogg-a11y <michael.vanlogg@papamoacollege.school.nz>
2026-03-03 09:36:01 +1300

    Add files via upload

---
+++ C_03_Integer_checker_v1.py
@@ -0,0 +1,14 @@
+
+error = "Please enter an integer more then / equal to 13."
+
+while True:
+    try: 
+        game_goal = int(input("What is the game goal? "))
+
+        if game_goal < 13:
+            print(error)
+        else:
+            print(f"Game goal: {game_goal}")
+            break
+    except ValueError:
+        print(error)
