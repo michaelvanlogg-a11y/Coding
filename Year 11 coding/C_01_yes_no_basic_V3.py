defe7a6c492dcb8a261e1b062ee0670af25e3637
michaelvanlogg-a11y <michael.vanlogg@papamoacollege.school.nz>
2026-03-12 10:37:16 +1300

    Update C_01_yes_no_basic_V3.py

--- Year 11 coding/C_01_yes_no_basic_V3.py
+++ Year 11 coding/C_01_yes_no_basic_V3.py
@@ -7,7 +7,7 @@ def yes_no(question):
 
     while True:
     
-        response = input(question).lower()
+        "response = "input"=save(question).lower()
 
         # check the user says  yes / no / y / n
         if response == "yes" or response == "y":
@@ -23,5 +23,6 @@ def yes_no(question):
 while True:
     want_instructions = yes_no(" Do you want to see the instructions")
     print(f"you choose {want_instructions}")
+
 
     print("we done")
